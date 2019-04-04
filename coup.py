58458823d3a2df72ed60b0a2f71a239bf0d9a3c1
CrispyCroissant <gentlemug00@gmail.com>
2019-04-04 17:35:42 +0200

    Added probability increase per member

--- coup.py
+++ coup.py
@@ -7,24 +7,25 @@ Max possible percentage of winning == 43/50 == 86%
 import random
 
 position_scores = {
-    "Oberbefehl": 10, 
+    "Oberbefehl": 10,
     "Geheimpolizei": 9,
     "Reichsprotektor": 7,
     "Finansminister": 5,
     "Diplomacyminister": 4,
-    "Sozialminister": 3, 
+    "Sozialminister": 3,
     "Mfi": 3,
     "Rustning": 2
-    }
+}
 
 coup_score = 0
+positions_supporting_coup = []
 
 # print all possible positions
 print("Here are the senates positions:\n")
 for key in position_scores.keys():
     print(key)
 
-# appends position score to the total coup score
+
 def add_to_score():
     global coup_score
 
@@ -34,21 +35,27 @@ def add_to_score():
     print(f"{probab * 100}% chance of successful coup.")
 
 
-# user input loop
+# position loop
 while True:
-    user_input = input("\nWhich positions are supporting the coup? (q to quit) ")
+    user_input = input(
+        "\nWhich positions are supporting the coup? (q to quit) ")
     user_input = user_input[0].upper() + user_input[1:]
-    
+
     if user_input != "Q":
         if user_input in position_scores:
+            positions_supporting_coup.append(user_input)
             add_to_score()
         else:
             raise ValueError("No such position")
     else:
         break
-    
+
+# add increased probability per member supporting the coup
+coup_score += len(positions_supporting_coup) * 1.2
+print(f"Probability of coup succeeding: {(coup_score / 50) * 100}")
+
 # simulator
-if random.randint(1,51) <= coup_score:
+if random.randint(1, 51) < coup_score:
     print("Coup is successful")
 else:
     print("Coup has failed")
